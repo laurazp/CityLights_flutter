880104ff1a021dafec78d49f905dc756c6a55d80
laura <69944724+laurazp@users.noreply.github.com>
2024-01-12 13:18:21 +0100

    MyLocationButton added to center camera on location in map

---
+++ lib/presentation/widget/my_location_button.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+
+class MyLocationButton extends StatelessWidget {
+  const MyLocationButton(
+      {super.key, required this.location, required this.mapController});
+
+  final LatLng location;
+  final MapController mapController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+        top: 16,
+        right: 16,
+        child: GestureDetector(
+          child: const Icon(Icons.my_location, color: Colors.blueGrey),
+          onTap: () {
+            mapController.move(location, 17.0);
+          },
+        ));
+  }
+}
